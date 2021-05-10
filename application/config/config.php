8077c41627ae33c72328c65ae842a1122326c3d4
abrs31 <abdulrahman.siahaan@gmail.com>
2021-05-10 08:20:52 +0700

    revisi base url config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/edukasi/';
+$config['base_url'] = 'http://localhost/edukasi-rest-server/';
 
 /*
 |--------------------------------------------------------------------------
